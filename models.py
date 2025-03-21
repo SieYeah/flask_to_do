b4ce4c2fd6404687d3ac82fdc787e1135d3968d1
SieYeah <159595731+SieYeah@users.noreply.github.com>
2025-03-22 00:00:09 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-
-# this is now in app.py file
-"""app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///tasks.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-"""
 db = SQLAlchemy()
 
 class Task(db.Model):
@@ -15,9 +8,9 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable = False)
     done = db.Column(db.Boolean, default = False)
 
-class User(db.Model)
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Colummn(db.String(28),  unique = True, nullable = False)
+    email = db.Column(db.String(28),  unique = True, nullable = False)
     password = db.Column(db.String(30), nullable = False)
 
     def set_password(self, password):
@@ -25,5 +18,3 @@ class User(db.Model)
 
     def check_password(self, password):
         self.password = check_password_hash(self.password, password)
-"""with app.app_context():
-    db.create_all()"""
