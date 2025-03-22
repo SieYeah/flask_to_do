8b505c6a3d03a84a6c88b27b9a239641f623c250
SieYeah <159595731+SieYeah@users.noreply.github.com>
2025-03-22 02:06:37 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from flask_marshmallow import Marshmallow
-from marshmallow import fields, ValidationError
+from marshmallow import fields, ValidationError, validates_schema
 from models import User
 
 
@@ -18,6 +18,10 @@ def validate_email(value):
         raise ValidationError("email cannot exceed 28 characters")
     if User.query.filter_by(email = value).first():
         raise ValidationError("You already have an account")
+
+
+
+
     
 
 
@@ -28,9 +32,25 @@ class TaskSchema(ma.Schema):
 class RegisterSchema(ma.Schema):
     email = fields.Str(required=True, validate = validate_email)
     password = fields.Str(required = True)
+class LoginSchema(ma.Schema):
+    email = fields.Str(required=True)
+    password = fields.Str(required = True)
+
+    @validates_schema
+    def validate_credentials(self, data, **kwargs):
+        user = User.query.filter_by(email = data.get("email")).first()
+
+        if not user:
+            raise ValidationError("bad email", field_name = "email")
+        if not user.check_password(data.get("password")):
+            raise ValidationError("bad password", field_name = "password")
+        
+        self.context["user"] = user
+
 
 
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
 register_schema = RegisterSchema()
+login_schema = LoginSchema()
 
